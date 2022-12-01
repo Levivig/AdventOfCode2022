4ff88c37815e9db8f89814b3091a45e42d2df2fa
Levente Vig <levente.vig@icloud.com>
2022-12-01 07:41:38 +0100

    ✨ Day 1 completed

--- day1/day1.py
+++ day1/day1.py
@@ -10,25 +10,33 @@ dirname = os.path.dirname(__file__)
 inputfile = os.path.join(dirname, 'input.txt')
 
 
-def part1(data):
+def part1(sums):
     print('part1:')
-    for line in data:
-        print(line)
+
+    print(max(sums))
 
 
-def part2(data):
+def part2(sums):
     print('part2:')
-    for line in data:
-        print(line)
+
+    top3 = sorted(sums, reverse=True)[:3]
+    print(sum(top3))
 
 
 def main():
     currentDay = os.path.basename(__file__).split('.')[0]
     print(currentDay)
     with open(inputfile) as f:
-        data = f.read().splitlines()
-        part1(data)
-        part2(data)
+        data = f.read()
+        data = data.split("\n\n")
+        elves = []
+        for idx, d in enumerate(data):
+            elfData = d.split("\n")
+            elfData = sum([int(food) for food in elfData])
+            elves.append(elfData)
+
+        part1(elves) # 72511
+        part2(elves) # 212117
 
 
 if __name__ == "__main__":
